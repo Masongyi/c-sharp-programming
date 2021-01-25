98d81832f0eaaac6ca50ac419bb77814e94104b9
SONG <36039296+Masongyi@users.noreply.github.com>
2021-01-25 10:22:21 +0100

    Adding Mouse,Pet,Album classes

---
+++ Abum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Album
+    {
+        private string albumName;
+        private string artistName;
+        private int numberOfTracks;
+      
+
+        private void ReadAlbum()
+        {   
+            Console.WriteLine("What is the name of your favorit music album?");
+            albumName = Console.ReadLine();
+        }
+
+        private void ReadArtist()
+        {
+            Console.WriteLine();
+            Console.WriteLine("What is the name of Artist or Brand for " + albumName + "?");
+            artistName = Console.ReadLine();
+        }
+
+        private void ReadTrackNumber()
+        {
+            Console.WriteLine();
+            Console.WriteLine("How many tracks dose " + albumName + " have?");
+            numberOfTracks = int.Parse(Console.ReadLine());
+        }
+        private void ReadSaveAlbum()
+        {
+            ReadAlbum();
+            ReadArtist();
+            ReadTrackNumber();
+            
+        }
+        private void DisplayAlbumInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Album Name: " + albumName);
+            Console.WriteLine("Artist/Brand: " + artistName);
+            Console.WriteLine("Number of Tracks: " + numberOfTracks);
+            Console.WriteLine();
+            Console.WriteLine("Enjoy listening!");
+        }
+        public void Start()
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine(time.ToString("yyyy-MM-dd h:mm:ss tt"));
+
+            Console.WriteLine("Starting Album Program!");
+            ReadSaveAlbum();
+            Console.WriteLine();
+            DisplayAlbumInfo();
+        }
+
+    }
+}
---
+++ Pet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Pet
+    {
+        private string name;
+        private int age;
+        private bool isFemale;
+
+        private void ReadName()
+        {
+            // read pet name
+            Console.WriteLine("What is the name of your pet?");
+            name = Console.ReadLine();
+        }
+
+        private void ReadAge ()
+        {
+            // read pet age 
+            Console.WriteLine("What is " + name + "'s age");
+            age = int.Parse(Console.ReadLine());
+        }
+
+        private void ReadGender()
+        {
+            // read y/n to gender question 
+            Console.WriteLine("Is " + name + "a female ? please enter y / n");
+            string respond = Console.ReadLine().Trim();
+
+            if ((respond =="y") || (respond =="Y"))
+                isFemale = true;
+            else
+                isFemale = false;
+        }
+
+
+        private void ReadAndSavePetData()
+        {
+
+            ReadName();
+            Console.WriteLine();
+            ReadAge();
+            Console.WriteLine();
+            ReadGender();
+
+        }
+        private void DisplayPetInfo()
+
+        {
+            string Genderinfo = isFemale ? name  + " is a good girl" :name  + "is a good boy";
+
+            string Petinfo = "Pet's name: " + name + ", Age: " + age;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Petinfo);
+            Console.WriteLine(Genderinfo);
+        }
+        public void Start()
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine(time.ToString("yyyy-MM-dd h:mm:ss tt"));
+            Console.WriteLine("Welcome to Pet Owner Application");
+
+            ReadAndSavePetData();
+            Console.WriteLine();
+            DisplayPetInfo();
+
+        }
+
+
+    }
+}
